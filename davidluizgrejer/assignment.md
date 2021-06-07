86099c91f44fb02e4df14bf4b4f2d9e784ad61e4
jacber03 <jacber@edu.umea.se>
2021-06-07 23:56:29 +0200

    testt

---
+++ davidluizgrejer/assignment.md
@@ -0,0 +1,36 @@
+Klient
+A client is a computer or a program that, as part of its operation, relies on sending a request to another program or a computer hardware or software that accesses a service made available by a server
+
+Server
+In computing, a server is a piece of computer hardware or software (computer program) that provides functionality for other programs or devices, called "clients". 
+
+DNS
+konverterar websidans domännamn med en ip adress
+
+TCP/IP
+en ip adress är ett nummer som 
+
+HTTP
+
+Webbsida
+Ett
+Webbläsare
+allows you to visit websites every 
+webpage runs through it 
+
+Webbserver
+
+Internet
+
+Sökmotor
+
+Paket
+
+Routning
+
+HTML
+text,bilder
+CSS
+colors,font, position
+JavaScript
+pop ups, 
---
+++ js/base.js
@@ -0,0 +1,211 @@
+function Info38(){
+    document.getElementById("namn").innerHTML = "#38 Rasmus Andersson";
+    document.getElementById("position").innerHTML = "Position: Back";
+    document.getElementById("född").innerHTML = "Född: 2003";
+    document.getElementById("moderklubb").innerHTML = "Moderklubb: Ersboda SK";
+    window.scrollTo(0, 325);
+}
+function Info39(){
+    document.getElementById("namn").innerHTML = "#39 Ludwig Olofsson";
+    document.getElementById("position").innerHTML = "Position: Back";
+    document.getElementById("född").innerHTML = "Född: 2003";
+    document.getElementById("moderklubb").innerHTML = "Moderklubb: Dorotea IF";
+    window.scrollTo(0, 325);
+}
+function Info40(){
+    document.getElementById("namn").innerHTML = "#40 Theo Lindberg";
+    document.getElementById("position").innerHTML = "Position: Målvakt";
+    document.getElementById("född").innerHTML = "Född: 2003";
+    document.getElementById("moderklubb").innerHTML = "Moderklubb: Ersmarks IK";
+    window.scrollTo(0, 325);
+}
+function Info41(){
+    document.getElementById("namn").innerHTML = "#41 Alexander Pliatsikos";
+    document.getElementById("position").innerHTML = "Position: Mittfältare";
+    document.getElementById("född").innerHTML = "Född: 2002";
+    document.getElementById("moderklubb").innerHTML = "Moderklubb: IFK Umeå";
+    window.scrollTo(0, 325);
+}
+function Info43(){
+    document.getElementById("namn").innerHTML = "#43 Mattias Nilsson";
+    document.getElementById("position").innerHTML = "Position: Back";
+    document.getElementById("född").innerHTML = "Född: 2002";
+    document.getElementById("moderklubb").innerHTML = "Moderklubb: Ersmarks IK";
+    window.scrollTo(0, 325);
+}
+function Info46(){
+    document.getElementById("namn").innerHTML = "#46 Victor Öberg";
+    document.getElementById("position").innerHTML = "Position: Back";
+    document.getElementById("född").innerHTML = "Född: 2002";
+    document.getElementById("moderklubb").innerHTML = "Moderklubb: IFK Umeå";
+    window.scrollTo(0, 325);
+}
+function Info48(){
+    document.getElementById("namn").innerHTML = "#48 William Skoglund";
+    document.getElementById("position").innerHTML = "Position: Mittfältare";
+    document.getElementById("född").innerHTML = "Född: 2003";
+    document.getElementById("moderklubb").innerHTML = "Moderklubb: Ersmarks IK";
+    window.scrollTo(0, 325);
+}
+function Info49(){
+    document.getElementById("namn").innerHTML = "#49 Elton Hedström";
+    document.getElementById("position").innerHTML = "Position: Forward";
+    document.getElementById("född").innerHTML = "Född: 2003";
+    document.getElementById("moderklubb").innerHTML = "Moderklubb: Ersmarks IK";
+    window.scrollTo(0, 325);
+}
+function Info54(){
+    document.getElementById("namn").innerHTML = "#54 John Carrick";
+    document.getElementById("position").innerHTML = "Position: Back";
+    document.getElementById("född").innerHTML = "Född: 2002";
+    document.getElementById("moderklubb").innerHTML = "Moderklubb: Gimonäs Umeå IF";
+    window.scrollTo(0, 325);
+}
+function Info56(){
+    document.getElementById("namn").innerHTML = "#56 Mortaza Safari";
+    document.getElementById("position").innerHTML = "Position: Mittfältare";
+    document.getElementById("född").innerHTML = "Född: 2003";
+    document.getElementById("moderklubb").innerHTML = "Moderklubb: Gimonäs Umeå IF";
+    window.scrollTo(0, 325);
+}
+function Info58(){
+    document.getElementById("namn").innerHTML = "#58 Olle Sandqvist";
+    document.getElementById("position").innerHTML = "Position: Forward";
+    document.getElementById("född").innerHTML = "Född: 2002";
+    document.getElementById("moderklubb").innerHTML = "Moderklubb: Sävar IK";
+    window.scrollTo(0, 325);
+}
+function Info59(){
+    document.getElementById("namn").innerHTML = "#59 David Ekman";
+    document.getElementById("position").innerHTML = "Position: Mittfältare";
+    document.getElementById("född").innerHTML = "Född: 2002";
+    document.getElementById("moderklubb").innerHTML = "Moderklubb: Tegs SK";
+    window.scrollTo(0, 325);
+}
+function Info62(){
+    document.getElementById("namn").innerHTML = "#62 Mikio Toyama";
+    document.getElementById("position").innerHTML = "Position: Back";
+    document.getElementById("född").innerHTML = "Född: 2003";
+    document.getElementById("moderklubb").innerHTML = "Moderklubb: Gimonäs Umeå IF";
+    window.scrollTo(0, 325);
+}
+function Info63(){
+    document.getElementById("namn").innerHTML = "#63 Nati Loha";
+    document.getElementById("position").innerHTML = "Position: Forward";
+    document.getElementById("född").innerHTML = "Född: 2003";
+    document.getElementById("moderklubb").innerHTML = "Moderklubb: Ersboda SK";
+    window.scrollTo(0, 325);
+}
+function Info65(){
+    document.getElementById("namn").innerHTML = "#65 Winukka Wiyayatunga";
+    document.getElementById("position").innerHTML = "Position: Forward";
+    document.getElementById("född").innerHTML = "Född: 2002";
+    document.getElementById("moderklubb").innerHTML = "Moderklubb: Gimonäs Umeå IF";
+    window.scrollTo(0, 325);
+}
+function Info66(){
+    document.getElementById("namn").innerHTML = "#66 Arvin Kunalic";
+    document.getElementById("position").innerHTML = "Position: Forward";
+    document.getElementById("född").innerHTML = "Född: 2003";
+    document.getElementById("moderklubb").innerHTML = "Moderklubb: Ersboda SK";
+    window.scrollTo(0, 325);
+}
+function Info67(){
+    document.getElementById("namn").innerHTML = "#67 Isak Pudas";
+    document.getElementById("position").innerHTML = "Position: Mittfältare";
+    document.getElementById("född").innerHTML = "Född: 2003";
+    document.getElementById("moderklubb").innerHTML = "Moderklubb: Gimonäs Umeå IF";
+    window.scrollTo(0, 325);
+}
+function Info71(){
+    document.getElementById("namn").innerHTML = "#71 Jacob Berglund";
+    document.getElementById("position").innerHTML = "Position: Mittfältere";
+    document.getElementById("född").innerHTML = "Född: 2003";
+    document.getElementById("moderklubb").innerHTML = "Moderklubb: Gimonäs Umeå IF";
+    window.scrollTo(0, 325);
+}
+function Info72(){
+    document.getElementById("namn").innerHTML = "#72 Hugo Enfors";
+    document.getElementById("position").innerHTML = "Position: Back";
+    document.getElementById("född").innerHTML = "Född: 2002";
+    document.getElementById("moderklubb").innerHTML = "Moderklubb: Ope IF";
+    window.scrollTo(0, 325);
+}
+function Info73(){
+    document.getElementById("namn").innerHTML = "#73 Theo Backsell";
+    document.getElementById("position").innerHTML = "Position: Back";
+    document.getElementById("född").innerHTML = "Född: 2003";
+    document.getElementById("moderklubb").innerHTML = "Moderklubb: Oklart";
+    window.scrollTo(0, 325);
+}
+function Info74(){
+    document.getElementById("namn").innerHTML = "#74 Lukas Karlsson";
+    document.getElementById("position").innerHTML = "Position: Forward";
+    document.getElementById("född").innerHTML = "Född: 2003";
+    document.getElementById("moderklubb").innerHTML = "Moderklubb: IFK Umeå";
+    window.scrollTo(0, 325);
+}
+function Info77(){
+    document.getElementById("namn").innerHTML = "#77 Jesper Lindbergh";
+    document.getElementById("position").innerHTML = "Position: Målvakt";
+    document.getElementById("född").innerHTML = "Född: 2003";
+    document.getElementById("moderklubb").innerHTML = "Moderklubb: IFK Umeå";
+    window.scrollTo(0, 325);
+}
+function Info80(){
+    document.getElementById("namn").innerHTML = "#80 Shallom Kidane";
+    document.getElementById("position").innerHTML = "Position: Forward";
+    document.getElementById("född").innerHTML = "Född: 2003";
+    document.getElementById("moderklubb").innerHTML = "Moderklubb: Robertsfors IK";
+    window.scrollTo(0, 325);
+
+}
+
+function sizeButtonS(){
+    document.getElementById("sizeButtonS").style.backgroundColor = "rgb(212, 211, 211)";
+    document.getElementById("sizeButtonM").style.backgroundColor = "whitesmoke";
+    document.getElementById("sizeButtonL").style.backgroundColor = "whitesmoke";
+    document.getElementById("sizeButtonXL").style.backgroundColor = "whitesmoke";
+}
+function sizeButtonM(){
+    document.getElementById("sizeButtonS").style.backgroundColor = "whitesmoke";
+    document.getElementById("sizeButtonM").style.backgroundColor = "rgb(212, 211, 211)";
+    document.getElementById("sizeButtonL").style.backgroundColor = "whitesmoke";
+    document.getElementById("sizeButtonXL").style.backgroundColor = "whitesmoke";
+}
+function sizeButtonL(){
+    document.getElementById("sizeButtonS").style.backgroundColor = "whitesmoke";
+    document.getElementById("sizeButtonM").style.backgroundColor = "whitesmoke";
+    document.getElementById("sizeButtonL").style.backgroundColor = "rgb(212, 211, 211)";
+    document.getElementById("sizeButtonXL").style.backgroundColor = "whitesmoke";
+}
+function sizeButtonXL(){
+    document.getElementById("sizeButtonS").style.backgroundColor = "whitesmoke";
+    document.getElementById("sizeButtonM").style.backgroundColor = "whitesmoke";
+    document.getElementById("sizeButtonL").style.backgroundColor = "whitesmoke";
+    document.getElementById("sizeButtonXL").style.backgroundColor = "rgb(212, 211, 211)";
+}
+
+function shirt(){
+    document.getElementById("shirtNum").innerHTML = document.getElementById("userNum").value
+    document.getElementById("shirtName").innerHTML = document.getElementById("userName").value
+}
+
+
+
+
+
+function fadeIn() {
+    var x = document.getElementById("ufcFakta");
+    var opacity = 0;
+    function fade () {
+        x.style.opacity = opacity;
+        opacity += 0.01;
+        if (opacity >= 1){
+            x.style.opacity = 1;
+            return true;
+        }
+        requestAnimationFrame(fade);
+    }
+    fade();
+}
